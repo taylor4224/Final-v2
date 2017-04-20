2d9b3820af8df1d1c33e28a8d4ccd5237fa4ad34
Alex Taylor <alex@Alex-MacBook-Pro.local>
2017-04-20 09:19:53 -0400

    It appears previous change didn't commit properly as previous changes are showing up as new changes on this file, and previous changes were not properly saved. This commit is due to Xcode behaving weirdly and I wish to uninstall and re-install. The error showing up is the auto-suggestion pop up when i type in a variable or function is no longer popping up. Also, to my knowledge with the previous save, nothing has changed however I am now getting a white screen on the simulator, even when previous incomplete code had a fps counter show up at least with a grey screen.

--- ICS4UI_final_cloudGame/GameScene.swift
+++ ICS4UI_final_cloudGame/GameScene.swift
@@ -18,63 +18,67 @@ class GameScene: SKScene {
     // touch location
     var targetLocation: CGPoint = .zero
     
+    var cam:SKCameraNode!
     
     override func didMove(to view: SKView) {
-        loadSceneNodes()
         
+        cam = SKCameraNode()
+        // need to set the scale by default somehow :/
+        
+        self.camera = cam
+        self.addChild(cam)
+
+        cam.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        
+        // preparing the map, making sure the forest background loaded, and that water tiles get placed
+        loadSceneNodes()
         setupWater()
     }
     
+    // making sure forest background loads
     func loadSceneNodes() {
-        guard let landBackground = childNode(withName: "landBackground")
-            as? SKTileMapNode else {
+        guard let landBackground = childNode(withName: "landBackground") as? SKTileMapNode else {
                 fatalError("Background node not loaded")
         }
         self.landBackground = landBackground
     }
     
+    // randomly placing water tiles on top of forest tiles
     func setupWater() {
-        let columns = 23
-        let rows = 13
+        let columns = 32
+        let rows = 24
         let size = CGSize(width: 64, height: 64)
         
-        // 1
         guard let tileSet = SKTileSet(named: "Water Tile") else {
             fatalError("Water Tile Set not found")
         }
         
-        // 2
         waterTileMap = SKTileMapNode(tileSet: tileSet,
                                        columns: columns,
                                        rows: rows,
                                        tileSize: size)
         
-        // 3
         addChild(waterTileMap)
         
-        // 4
         let tileGroups = tileSet.tileGroups
         
-        // 5
         guard let waterTile = tileGroups.first(where: {$0.name == "Water"}) else {
             fatalError("No Water tile definition found")
         }
         
-        // 6
         let waterSources = 10
         
-        // 7
         for _ in 1...waterSources {
             
-            // 8
             let column = Int(arc4random_uniform(UInt32(columns)))
             let row = Int(arc4random_uniform(UInt32(rows)))
             
-            // 9
             let tile = waterTile
             
-            // 10
             waterTileMap.setTileGroup(tile, forColumn: column, row: row)
+            
+            //let tile = water
         }
     }
+    //func touchesBegan
 }
